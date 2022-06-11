0432c57123ee633871799cd21e786bf91924b7a9
Daniel Williams <williamsoligodaniel@gmail.com>
2022-06-11 07:41:42 +0000

    lastLetter

---
+++ lastLetter.js
@@ -0,0 +1,8 @@
+function lastLetter(word) {
+  let last = word[word.length-1];
+  return last
+}
+console.log(lastLetter("hello")); //=> "o"
+console.log(lastLetter("goodbye!")); //=> "!"
+console.log(lastLetter("ZeltoiD")); //=> "D"
+console.log(lastLetter("I love Javascript")); //=> "t"
