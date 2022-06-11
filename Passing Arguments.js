b44bccbebe012bc1c6cc92175bd4ee7d1083f805
Daniel Williams <williamsoligodaniel@gmail.com>
2022-06-11 07:53:01 +0000

    Passing Arguments

---
+++ Passing Arguments.js
@@ -0,0 +1,16 @@
+const x = 3;
+
+function isX1(num) {
+    const x = 5;
+    return num === x;
+}
+
+function isX2(num) {
+    return num === x;
+}
+
+const one = isX1(5);
+const two = isX2(3);
+
+console.log(one); // should log: true
+console.log(two); // should log: true
