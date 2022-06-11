4cec49a11b0eae8f5a29d4845e6b1df9e1132e9b
Daniel Williams <williamsoligodaniel@gmail.com>
2022-06-11 07:33:51 +0000

    wereAwesome

---
+++ wereAwesome.js
@@ -0,0 +1,9 @@
+function wereAwesome(you, yourBuddy) {
+  let remarks = " "+ yourBuddy +" and "+ you +" are awesome!";
+  return remarks;
+}
+
+console.log(wereAwesome("Dave", "Will")); // => "Will and Dave are awesome!"
+console.log(wereAwesome("Victoria", "Jenny")); // => "Jenny and Victoria are awesome!"
+console.log(wereAwesome("Chris", "Jac")); // => "Jac and Chris are awesome!"
+console.log(wereAwesome("Phillip", "Skyler")); // => "Skyler and Phillip are awesome!"
